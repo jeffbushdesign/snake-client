206aa0c958706642bca2197709ad68078da8db35
Jeff Bush <jeffbushdesign@gmail.com>
2022-02-21 15:45:23 -0500

    Changed plays.js

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
-const net = require("net");
+
+
+
 const { connect } = require("./client.js");
-
-
-// MOVED TO CLIENT.JS
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: 'localhost', // IP address here,
-//     port: 50541,// PORT number here,
-//   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   conn.on("connect", () => {
-//     // code that does something when the connection is first established
-//   });
-
-//   return conn;
-// };
 
 console.log("Connecting ...");
 connect();
+
+test;
