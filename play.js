89ce7487b038d93c0bb83b9f78c4f7165ee399ce
jeffbushdesign <jeffbushdesign@gmail.com>
2022-02-21 21:35:50 -0500

    Updated play.js

--- play.js
+++ play.js
@@ -5,3 +5,34 @@ const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+
+const handleUserInput = function (key) {
+  // your code here
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+setupInput();
+
+// on any input from stdin (standard input), output a "." to stdout
+// stdin.on('data', (key) => {
+//   process.stdout.write('.');
+// });
+
+// stdin deals with "standard input", much like how stdout is for "standard output".
+// We use the on method on stdin to register a callback
